b837121c2cbd5badc34621e9f7eeb993409d7979
vardose <giraudonaxel@gmail.com>
2024-01-11 17:54:45 +0100

    adding of the Chat class to make it more easy to use

--- src/app.js
+++ src/app.js
@@ -1,54 +1,9 @@
 const socket = require('./socket.js');
-const { LiveChat } = require("youtube-chat");
+const Chat = require ("./Chat.js")
 
-const liveChats = {};
+let test = ["UCHoLuRQVGaz5lkdnIZWB8Pw"]
 
-async function startChat(channelId) {
-  if (!liveChats[channelId]) {
-    const liveChat = new LiveChat({ channelId });
-
-    liveChat.on("start", () => {
-      console.log(`Chat for channel ${channelId} started`);
-      liveChat.start();
-    });
-
-    liveChat.on("end", () => {
-      console.log(`Chat for channel ${channelId} ended`);
-      liveChat.stop();
-      socket.disconnect();
-      delete liveChats[channelId];
-    });
-
-    liveChat.on("chat", (chatItem) => {
-      socket.emitEvent("message", chatItem);
-    });
-
-    liveChat.on("error", (error) => {
-      console.error(error);
-    });
-
-    const ok = await liveChat.start();
-    if (!ok) {
-      console.log(`Failed to start chat for channel ${channelId}.`);
-      return false;
-    }
-
-    liveChats[channelId] = liveChat;
-    return true;
-  } else {
-    console.log(`Chat for channel ${channelId} is already running.`);
-    return false;
-  }
-}
-
-async function stopChat(channelId) {
-  if (liveChats[channelId]) {
-    liveChats[channelId].stop();
-    delete liveChats[channelId];
-    console.log(`Chat for channel ${channelId} stopped.`);
-  } else {
-    console.log(`Chat for channel ${channelId} is not running.`);
-  }
-}
-
-module.exports = { startChat, stopChat };
+test.forEach(element => {
+  let chat = new Chat.Chat(element)
+  chat.startChat()
+});
--- src/socket.js
+++ src/socket.js
@@ -1,26 +1,19 @@
+const socket = require('socket.io-client')('http://localhost:4000');
 
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
+function emitEvent(eventName, data) {
+    if (socket.connected)
+      socket.emit(eventName, data);
+    else
+      console.log('Le socket n\'est pas connecté.');
+};
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+function onEvent(eventName, callback) {
+    socket.on(eventName, callback);
+};
 
-io.on('connection', (socket) => {
-  console.log('Un client s\'est connecté');
+function disconnect() {
+    socket.disconnect();
+    console.log('Déconnecté du serveur WebSocket');
+};
 
-  // Gérer les événements du client
-  socket.on('message', (data) => {
-    console.log('Message reçu du client:', data);
-  });
-
-  // Gérer la déconnexion du client
-  socket.on('disconnect', () => {
-    console.log('Un client s\'est déconnecté');
-  });
-});
-
-server.listen(4001, () => {
-  console.log('Le serveur WebSocket est en écoute sur le port 4000');
-});
+module.exports = { emitEvent, onEvent, disconnect };
