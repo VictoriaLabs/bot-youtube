87b1b263cfc9f6af12feb0e76e9150b1cef03252
loanf <loanfrancois1@gmail.com>
2024-02-19 09:37:57 +0100

    init glitchtip

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
+const Sentry = require("@sentry/node");
 const express = require('express')
 const { request: Req } = require('express')
 const { response: Res } = require('express')
 
 const app = express();
 const port = 8000;
+
+Sentry.init({ dsn: "https://30dba8cc80054e25b9b5da7b58d41e00@glitchtip.victorialabs.site/5" });
 
 app.get('/', (req: typeof Req, res: typeof Res) => {
   res.send('Express Bot TS');
